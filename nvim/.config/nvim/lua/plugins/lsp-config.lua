759f7bc72e6fa6b17df6666458369cacee22f31b
Ziqi Wang <wangziqi2385823041@outlook.com>
2025-03-16 20:16:11 +0200

    more on lsp-config

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,7 +1,56 @@
 return {
-  "williamboman/mason.nvim",
-  config = function()
-    require("mason").setup()
-  end
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed =  { "lua_ls", "bashls", "dockerls", "docker_compose_language_service", "ast_grep"}
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({
+        -- start copied from document
+        on_init = function(client)
+          if client.workspace_folders then
+            local path = client.workspace_folders[1].name
+            if path ~= vim.fn.stdpath('config') and (vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc')) then
+              return
+            end
+          end
+
+          client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+            runtime = {
+              version = 'LuaJIT'
+            },
+            -- Make the server aware of Neovim runtime files
+            workspace = {
+              checkThirdParty = false,
+              library = {
+                vim.env.VIMRUNTIME
+              }
+            },
+            diagnostics = {
+              groupSeverity = {
+                codeStyle = "None",
+                unused = "None",
+              },
+              globals = { "vim" },
+            }
+          })
+        end,
+        -- end copied from document
+      })
+      lspconfig.ast_grep.setup({})
+    end
+  }
 }
 
