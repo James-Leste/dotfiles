1033deb7ea91570c096570b5563fceb1e6d9134f
Ziqi Wang <wangziqi2385823041@outlook.com>
2025-03-17 00:39:39 +0200

    add autocomplete for python and diagnostic

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -9,48 +9,50 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed =  { "lua_ls", "bashls", "dockerls", "docker_compose_language_service", "ast_grep"}
+        ensure_installed =  { "lua_ls", "basedpyright", "ruff"}
       })
     end
   },
   {
     "neovim/nvim-lspconfig",
+    keys = {
+      { "gd", vim.lsp.buf.definition, desc = "Goto Definition" },
+      { "gr", vim.lsp.buf.references, desc = "Goto References" },
+      { "<leader>ca", vim.lsp.buf.code_action, desc = "Code Action" },
+      { "<C-f>", vim.lsp.buf.format, desc = "Format File" },
+    },
     config = function()
+      vim.diagnostic.config({
+        signs = false,
+      })
+      vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
+      local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({
-        -- start copied from document
-        on_init = function(client)
-          if client.workspace_folders then
-            local path = client.workspace_folders[1].name
-            if path ~= vim.fn.stdpath('config') and (vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc')) then
-              return
-            end
-          end
-
-          client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-            runtime = {
-              version = 'LuaJIT'
-            },
-            -- Make the server aware of Neovim runtime files
-            workspace = {
-              checkThirdParty = false,
-              library = {
-                vim.env.VIMRUNTIME
-              }
-            },
+        capabilities = lsp_capabilities,
+        settings = {
+          Lua = {
             diagnostics = {
-              groupSeverity = {
-                codeStyle = "None",
-                unused = "None",
-              },
               globals = { "vim" },
             }
-          })
-        end,
-        -- end copied from document
+          }
+        }
       })
-      lspconfig.ast_grep.setup({})
-    end
-  }
+
+
+      lspconfig.basedpyright.setup({
+        capabilities = lsp_capabilities,
+        settings = {
+          python = {
+            pythonPath = vim.fn.exepath("python"),
+          }
+        }
+      })
+
+      lspconfig.ruff.setup({
+      })
+
+    end,
+  },
 }
 
