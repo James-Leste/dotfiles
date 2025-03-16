70e9cf8d8c12f3128817d0c9b6b5f0952257b9b0
Ziqi Wang <wangziqi2385823041@outlook.com>
2025-03-16 19:24:03 +0200

    add git plugins

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -1,14 +1,22 @@
-return {  
-  "f-person/git-blame.nvim",
-  event = "VeryLazy",
-  config = function()
-    vim.g.gitblame_message_template = '<author> • <date> • <summary> • <sha>'
-    vim.g.gitblame_date_format = "%r"
-    vim.g.gitblame_highlight_group = "Comment"
-    vim.g.gitblame_set_extmark_options = {
-      hl_mode = "combine",
-    }
-    vim.g.gitblame_delay = 0 
-  end
+return {
+  {
+    "f-person/git-blame.nvim",
+    event = "VeryLazy",
+    config = function()
+      vim.g.gitblame_message_template = '<author> • <date> • <summary> • <sha>'
+      vim.g.gitblame_date_format = "%r"
+      vim.g.gitblame_highlight_group = "Comment"
+      vim.g.gitblame_set_extmark_options = {
+        hl_mode = "combine",
+      }
+      vim.g.gitblame_delay = 0
+    end
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup()
+    end
+  }
 }
 
