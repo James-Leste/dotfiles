75cd5efd14f1c8605cd839562865e37a43455083
Ziqi Wang <wangziqi2385823041@outlook.com>
2025-06-18 22:03:58 +0000

    add nvim context and tmux color support

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,11 +1,24 @@
 return {
-  "nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
-  config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      ensure_installed = {"lua", "javascript", "python", "json", "ssh_config", "yaml", "bash", "markdown_inline", "c", "gitignore", "helm"},
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end
+  {
+    "nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
+    config = function()
+      local config = require("nvim-treesitter.configs")
+      config.setup({
+        ensure_installed = {"lua", "javascript", "python", "json", "ssh_config", "yaml", "bash", "markdown_inline", "c", "gitignore", "helm"},
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end
+  },
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    config = function()
+      local config = require("treesitter-context")
+      config.setup({
+        enable = true,
+        mode = 'topline',
+        line_number = true,
+      })
+    end
+  }
 }
