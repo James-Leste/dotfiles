2a307f38005be007582165eb401b75fd3fc4853a
Ziqi Wang <ziqi.wang@nokia.com>
2025-07-09 12:38:11 +0300

    add outline plugin

---
+++ nvim/.config/nvim/lua/plugins/outline.lua
@@ -0,0 +1,15 @@
+return {
+  "hedyhli/outline.nvim",
+  lazy = true,
+  cmd = { "Outline", "OutlineOpen" },
+  keys = { -- Example mapping to toggle outline
+    { "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
+  },
+  opts = {
+    outline_window = {
+      width = 15,
+      relative_width = true,
+    }    -- Your setup opts here
+  },
+}
+
