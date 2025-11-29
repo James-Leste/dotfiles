a7c2a4ae715ba1dff423fca314b4deb1dcaa4635
Ziqi Wang <wangziqi2385823041@outlook.com>
2025-11-29 13:46:19 +0200

    add onYank highlight

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -8,6 +8,14 @@ vim.keymap.set('n', '<C-d>', '<C-d>zz', { noremap = true, silent = true })
 vim.keymap.set('n', '<C-u>', '<C-u>zz', { noremap = true, silent = true })
 vim.keymap.set('n', 'N', 'Nzz', { noremap = true, silent = true })
 vim.keymap.set('n', 'n', 'nzz', { noremap = true, silent = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
 
 
 vim.g.mapleader = " "
