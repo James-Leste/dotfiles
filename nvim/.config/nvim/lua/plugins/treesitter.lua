9fb0e73550ec6fe2230978607710041241775a66
Ziqi Wang <wangziqi2385823041@outlook.com>
2026-01-20 23:03:40 +0200

    add go treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
     config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
-        ensure_installed = {"lua", "javascript", "python", "json", "ssh_config", "yaml", "bash", "markdown_inline", "c", "gitignore", "helm"},
+        ensure_installed = {"go", "lua", "javascript", "python", "json", "ssh_config", "yaml", "bash", "markdown_inline", "c", "gitignore", "helm"},
         highlight = { enable = true },
         indent = { enable = true },
       })
